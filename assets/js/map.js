6115bb979cac6feb3235dd663ef73eb71e387d67
Robertl231 <rloughran67@gmail.com>
2021-05-25 21:21:33 +0000

    added the last of us trailer to game trailer section

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,9 +1,9 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 4,
         center : {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 52.663420,
+            lng: -8.626619
         }
     });
 }
