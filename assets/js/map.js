4f0a52df05d42684ffe6be1091d5a9f98b1f15e5
Robertl231 <rloughran67@gmail.com>
2021-05-26 22:59:58 +0000

    added location of one store to test, also added visual markers for stores

--- assets/js/map.js
+++ assets/js/map.js
@@ -6,6 +6,26 @@ function initMap() {
             lng: -8.626619
         }
     });
+
+    var labels = "ABDEFGHIJKLMNOPQRSTUVWXYZ";
+var locations = [
+    {lat: 52.639506114300495, lng: -8.646336526169451},
+   // {lat:  , lng: },
+   // {lat:  , lng: },
+   // {lat:  , lng: },
+];
+
+var markers = locations.map(function(location, i) {
+         return new google.maps.Marker ({
+             position: location,
+             label: labels[i % labels.length]
+         }) 
+   })
+
+   new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
 }
 
-var labels = "ABDEFGHIJKLMNOPQRSTUVWXYZ"
+
