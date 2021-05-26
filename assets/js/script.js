b346f8b0c119e97456a1126cd6b7a954a2e08500
Robertl231 <rloughran67@gmail.com>
2021-05-26 23:36:58 +0000

    added function to script.js to ensure video trailers dont load when you first visit the page

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//dont want videos to load straight away, only when links are clicked should they appear
+window.onload = function() {
+    document.getElementById("videos").style.display="none";
+}
