8118a9778d2bc6739d1991484083323a4c0e7d57
Robertl231 <rloughran67@gmail.com>
2021-05-27 00:26:52 +0000

    changed how trailers and mapps loaded in as was havving trouble with certain divs loading and other divs not loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,18 @@
-//dont want videos to load straight away, only when links are clicked should they appear
+//dont want videos to load straight away, only when links are clicked should they appear  
+//also don't want google map to load straight away
 window.onload = function() {
-    document.getElementById("videos").style.display="none";
+    document.getElementById("uncharted-trailer").style.display="none";
+    document.getElementById("wtb-map").style.display="none";
 }
+function unchartedInfo() {
+    let x = document.getElementById("uncharted-trailer");
+    let y = document.getElementById("wtb-map");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    if (y.style.display === "none") {
+        y.style.display = "block"
+    }
+}
+
+
