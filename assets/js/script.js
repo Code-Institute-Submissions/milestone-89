e644b47a01cbbf9f7eea7b6ddb10c82301fd6823
Robertl231 <rloughran67@gmail.com>
2021-05-27 00:41:08 +0000

    wrote code in script.js to ensure trailer videos only appear on their own without other videos on top or under them when appropriate link is clicked, google map also appears with each video

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,57 @@ window.onload = function() {
 function tlouInfo() {
     let x = document.getElementById("tlou-trailer");
     let y = document.getElementById("wtb-map");
+    let z = document.getElementById("gow-trailer");
+    let t = document.getElementById("uncharted-trailer");
     if (x.style.display === "none") {
         x.style.display = "block";
     }
     if (y.style.display === "none") {
         y.style.display = "block"
+    }
+    if (z.style.display === "block") {
+        z.style.display = "none";
+    }
+    if (t.style.display === "block") {
+        t.style.display = "none";
     }
 }
 
 function gowInfo() {
     let x = document.getElementById("gow-trailer");
     let y = document.getElementById("wtb-map");
+    let z = document.getElementById("tlou-trailer");
+    let t = document.getElementById("uncharted-trailer");
     if (x.style.display === "none") {
         x.style.display = "block";
     }
     if (y.style.display === "none") {
         y.style.display = "block"
+    }
+    if (z.style.display === "block") {
+        z.style.display = "none";
+    }
+    if (t.style.display === "block") {
+        t.style.display = "none";
     }
 }
 
 function unchartedInfo() {
     let x = document.getElementById("uncharted-trailer");
     let y = document.getElementById("wtb-map");
+    let z = document.getElementById("gow-trailer");
+    let t = document.getElementById("tlou-trailer");
     if (x.style.display === "none") {
         x.style.display = "block";
     }
     if (y.style.display === "none") {
         y.style.display = "block"
+    }
+    if (z.style.display === "block") {
+        z.style.display = "none";
+    }
+    if (t.style.display === "block") {
+        t.style.display = "none";
     }
 }
 
